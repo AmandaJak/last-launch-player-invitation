d226e8e2176482da82da91f74d9f62903b611d70
amoja <amanda.o.jakobsen@gmail.com>
2026-05-05 22:05:58 +0200

    Updated layout to be light

---
+++ scripts/distrbute_roles.py
@@ -0,0 +1,6 @@
+## Script to distribute character roles between players
+
+characters = [
+    'Alex Ray', 'kind_mind', 'Sam Marin', 'Lior Vangard', 'Niko Byte', 'Ari Lind', 'Vespar', 'Morgan Page', 'Joss Elling', 'Robin Ray']
+players = [
+    'Emma', 'Christoffer', 'Michelle', 'Christina', 'Iben', 'KP', 'Mads', 'Christian']
